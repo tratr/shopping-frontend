34a2fed19a1fc62593a70bd058d9c220103c0c75
Trường Lê <travistrle@gmail.com>
2022-12-24 00:21:56 -0500

    add category (#2)

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,11 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
-import NxWelcome from './nx-welcome';
+import CategoryComponent from './category/CategoryComponent';
 
 export function App() {
   return (
     <>
-      <NxWelcome title="shopping-frontend" />
+      <CategoryComponent />
       <div />
     </>
   );
---
+++ src/app/category/CategoryComponent.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import styles from './category.module.scss';
+import { Category } from '../types';
+
+export default function CategoryComponent() {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState<Category[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const { data: response } = await axios.get(
+          '/api/v1/categories?page=0&size=20'
+        );
+        console.log(`------> ${JSON.stringify(response)}`);
+        setData(response);
+      } catch (error) {
+        console.error(error);
+      }
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <div>Hello How Are You</div>{' '}
+      <div>
+        {loading && <div>Loading</div>}
+        {!loading && (
+          <div>
+            <h2>Category</h2>
+            <ul>
+              {data.map((item) => (
+                <li>{item.name}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}

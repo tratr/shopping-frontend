16d51fcbbbfa6f37fa8586b870aadd01a1b40c48
Truong Le <truong.le@nuvoair.com>
2022-12-23 22:46:24 -0600

    add category

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
-import Category from './category';
+import Category from './category/category';
 
 export function App() {
   return (
--- src/app/category/category.tsx
+++ src/app/category/category.tsx
@@ -1,3 +1,5 @@
+import styles from './category.module.scss';
+
 export function Category() {
   return <div>Hello How Are You</div>;
 }
